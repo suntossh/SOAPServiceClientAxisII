e75b32fd2d49140b443ca6a8d3d432ee3b68d80b
Suntossh <suntossh.numerouno@gmail.com>
2016-09-11 19:08:50 -0400

    Adding Axis2 SOAP Client (jax-ws implementation), here client is a bit tricky. it uses Static Inner Class to Service and its Param Object and then calls get Response to get the output

---
+++ src/com/suntossh/ws/soap/axis2/service/AxisIIJaxRSSOAPClient.java
@@ -0,0 +1,32 @@
+package com.suntossh.ws.soap.axis2.service;
+
+import java.rmi.RemoteException;
+
+public class AxisIIJaxRSSOAPClient {
+
+	public static void main(String[] args) throws RemoteException {
+		CalculatorServiceStub stub = new CalculatorServiceStub();
+
+		CalculatorServiceStub.TestService testServiceParams = new CalculatorServiceStub.TestService();
+		testServiceParams.setYourName("Suntossh");
+
+		CalculatorServiceStub.TestServiceResponse testServiceResponse = stub.testService(testServiceParams);
+		System.out.println("Axis 2 TestServicea Response is :" + testServiceResponse.get_return());
+
+		CalculatorServiceStub.Add addParams = new CalculatorServiceStub.Add();
+		addParams.setX(10);
+		addParams.setY(20);
+
+		CalculatorServiceStub.AddResponse addResponse = stub.add(addParams);
+		System.out.println("Axis 2 Add Response is :" + addResponse.get_return());
+
+		CalculatorServiceStub.Sub subParams = new CalculatorServiceStub.Sub();
+		subParams.setX(10);
+		subParams.setY(20);
+
+		CalculatorServiceStub.SubResponse subResponse = stub.sub(subParams);
+		System.out.println("Axis 2 Sub Response is :" + subResponse.get_return());
+
+	}
+
+}
